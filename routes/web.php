717f282d579e8fe8d2e19c32776b192c4b5affda
Ahmad Athaullah <a-athaullah@users.noreply.github.com>
2025-12-06 09:35:37 +0700

    move root directory from public
    move root directory from public

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', 'Auth\LoginController@showLoginForm');
 
 Route::get('/login', 'Auth\LoginController@showLoginForm');
 
