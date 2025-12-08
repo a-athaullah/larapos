c806d601cfbb1eaaa5e6d3d50d4b8d9ec7bc239f
Ahmad Athaullah <a-athaullah@users.noreply.github.com>
2025-12-08 22:04:28 +0700

    Remove scustomer on form sales/create and add notes

--- resources/views/sales/create.blade.php
+++ resources/views/sales/create.blade.php
@@ -4,7 +4,7 @@
             <div class="col-sm-12">
                 <h1 class="title text-dark"> <svg class="bi bi-cart3" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
-                    </svg> Point Of Sale {{ $store->name }}</h1>
+                    </svg> Point Of Sale </h1>
             </div>
             <div class="col-12 text-center">
                 <h5>Income to day: <strong>Rp.{{ $totalSalesPerDay }}</strong></h5>
@@ -23,9 +23,11 @@
                 <div class="card card-dark">
                     <div class="card-body">
                         <div id="js-requests-messages"></div> @if($errors->any())<div class="alert alert-danger"> @foreach($errors->all() as $error) - {{ $error }} <br> @endforeach</div> @endif<form method="post" action="#" id="createSaleForm">
-                            <div class="form-group"> <label for="rfc">Customer</label> <select id="rfc" name="rfc" class="form-control">
-                                    <option value="">Select Customer</option> @foreach($clients as $client)<option value="{{ $client->rfc }}"> {{ $client->name }}</option> @endforeach
-                                </select></div>
+                            <div class="form-group"> 
+                                <label for="rfc">Notes</label> 
+                                <textarea id="sales_notes" name="notes" class= "form-control" placeholder="Put transaction notes here" style= "resize: none; line-height: 0.75;"></textarea>
+                                
+                            </div>
                             <div class="form-group">
                                 <table class="table">
                                     <thead>
