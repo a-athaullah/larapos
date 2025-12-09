c4758f075f21ffbfd06d05013dec32717949def5
Ahmad Athaullah <a-athaullah@users.noreply.github.com>
2025-12-09 13:03:05 +0700

    add refference to other models on table Stores

--- app/Store.php
+++ app/Store.php
@@ -2,10 +2,29 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-    use HasFactory;
+    protected $primaryKey = 'store_id';
+
+    protected $fillable = [
+        'name', 'description'
+    ];
+
+    public function users(){
+        return $this->hasMany('App\User');
+    }
+
+    public function categories(){
+        return $this->hasMany('App\Category');
+    }
+
+    public function products(){
+        return $this->hasMany('App\Product');
+    }
+
+    public function sales(){
+        return $this->hasMany('App\Sale');
+    }
 }
