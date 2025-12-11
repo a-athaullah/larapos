18d01eb8b545968ed3d59fb02457a16d2ef711ac
Ahmad Athaullah <a-athaullah@users.noreply.github.com>
2025-12-11 09:06:03 +0700

    move default path

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,7 +22,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
-    public const CASHIER = '/sales/create';
+    public const CASHIER = '/sales';
     /**
      * Define your route model bindings, pattern filters, etc.
      *
