843a52588e2b0de7435c299f2d1db1eb252a86da
Ahmad Athaullah <a-athaullah@users.noreply.github.com>
2025-12-12 17:23:39 +0700

    fix script load bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <script src="{{asset('js/lara.js')}}" defer></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
@@ -28,7 +29,6 @@
 </head>
 
 <body class="bg-light" x-data='{ct: "Categories", pd: "Products", ads: "Add User", acs: "Add Customer", dbps: "Databased Products", acg: "Add Categories", cnpdt: " Create New Product", apd: "Add Products", title: "Mayess", nav1: "Home", cc: "Create Categories", nav2: "Doc", cn: "Category Name", nav3: "Data", cnct: "Create New Categories", sv: "Save Data", ctndb: "Categories databased", nav4: "Transaction", ncsa: "New Customer", inc: "Income", nav5: "Report", pro: "PRO", rpt: "Report", rptdm: "Report details menu", nav6: "WebApp", xpr: "Export Report", pap: "Premium App", db: "Database",dbs: "Register Database in here", pfc: "Profit Income", us: "User", gpr: "Group Report", pdpr: "Product Report", cs: "Customer", nus: "Create new user", usdb: "User Databased Management", usb: "User Report", cdmb: "Customer Databased Management", csr: "Customer Report"}'>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <div class="wrapper">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-white"> 
             <a class="navbar-brand" href="/" style="margin-left:1em;">{{ $storeName }}</a> 
@@ -80,7 +80,6 @@
     <script src="{{asset('js/app.js')}}"></script><script src='https://cdn.jsdelivr.net/npm/vue/dist/vue.js'></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 
 </html>
